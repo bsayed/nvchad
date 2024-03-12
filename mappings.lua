d762c5defe19bd429cd584eb8fac796608096d8a
Bassam Sayed <bassam.sayed@worldcoin.org>
2024-03-12 10:38:49 -0400

    Added lspsaga and many key bindings.

--- mappings.lua
+++ mappings.lua
@@ -11,11 +11,25 @@ M.general = {
         require("conform").format()
       end,
       "formatting",
-    }
-
+    },
+    ["<leader>lo"] = { ":Lspsaga outline<CR>", "lspsaga show outline" },
+    ["<leader>ld"] = { ":Lspsaga peek_definition<CR>", "lspsaga peek definition" },
+    ["<leader>lt"] = { ":Lspsaga peek_type_definition<CR>", "lspsaga peek type definition" },
+    ["<leader>la"] = { ":Lspsaga code_action<CR>", "lspsaga code action" },
+    ["<leader>lr"] = { ":Lspsaga rename<CR>", "lspsaga rename" },
+    ["<leader>lh"] = { ":Lspsaga hover_doc<CR>", "lspsaga hover doc" },
+    ["<leader>lc"] = { ":Lspsaga incoming_calls<CR>", "lspsaga incoming calls" },
+    ["<leader>lg"] = { ":Lspsaga outgoing_calls<CR>", "lspsaga outgoing calls" },
+    ["<leader>lf"] = { ":Lspsaga finder<CR>", "lspsaga find references" },
+    ["<leader>lj"] = { ":Lspsaga diagnostic_jump_next<CR>", "lspsaga diagnostic jump next" },
+    ["<leader>lk"] = { ":Lspsaga diagnostic_jump_prev<CR>", "lspsaga diagnostic jump prev" },
+    ["<C-Right>"] = { ":vertical resize -5<CR>", "increase window size to the right" },
+    ["<C-Left>"] = { ":vertical resize +5<CR>", "decrease window size to the left" },
+    ["<C-Up>"] = { ":resize -5<CR>", "decrease window size upwords" },
+    ["<C-Down>"] = { ":resize +5<CR>", "increase window size downwords" },
   },
   v = {
-    [">"] = { ">gv", "indent"},
+    [">"] = { ">gv", "indent" },
   },
 }
 
--- plugins.lua
+++ plugins.lua
@@ -133,7 +133,7 @@ local plugins = {
           auto_trigger = true,
           debounce = 75,
           keymap = {
-            accept = "<Tab>",
+            accept = "<C-Right>",
             accept_word = false,
             accept_line = false,
             next = "<M-]>",
@@ -173,65 +173,68 @@ local plugins = {
       local formatters = require("format-on-save.formatters")
       format_on_save.setup({
         exclude_path_patterns = {
-        "/node_modules/",
-        ".local/share/nvim/lazy",
-      },
-      formatter_by_ft = {
-        css = formatters.lsp,
-        html = formatters.lsp,
-        java = formatters.lsp,
-        json = formatters.lsp,
-        lua = formatters.lsp,
-        markdown = formatters.prettierd,
-        openscad = formatters.lsp,
-        rust = formatters.lsp,
-        scad = formatters.lsp,
-        scss = formatters.lsp,
-        sh = formatters.shfmt,
-        terraform = formatters.lsp,
-        typescript = formatters.prettierd,
-        typescriptreact = formatters.prettierd,
-        yaml = formatters.lsp,
-
-        -- Add your own shell formatters:
-        myfiletype = formatters.shell({ cmd = { "myformatter", "%" } }),
-
-        -- Add lazy formatter that will only run when formatting:
-        my_custom_formatter = function()
-          if vim.api.nvim_buf_get_name(0):match("/README.md$") then
-            return formatters.prettierd
-          else
-            return formatters.lsp()
-          end
-        end,
-
-        -- Add custom formatter
-        filetype1 = formatters.remove_trailing_whitespace,
-        filetype2 = formatters.custom({ format = function(lines)
-          return vim.tbl_map(function(line)
-            return line:gsub("true", "false")
-          end, lines)
-        end}),
-
-        -- Concatenate formatters
-        python = {
-          formatters.remove_trailing_whitespace,
-          formatters.shell({ cmd = "tidy-imports" }),
-          formatters.black,
-          formatters.ruff,
+          "/node_modules/",
+          ".local/share/nvim/lazy",
         },
+        formatter_by_ft = {
+          css = formatters.lsp,
+          html = formatters.lsp,
+          java = formatters.lsp,
+          json = formatters.lsp,
+          lua = formatters.lsp,
+          markdown = formatters.prettierd,
+          openscad = formatters.lsp,
+          rust = formatters.lsp,
+          scad = formatters.lsp,
+          scss = formatters.lsp,
+          sh = formatters.shfmt,
+          terraform = formatters.lsp,
+          typescript = formatters.prettierd,
+          typescriptreact = formatters.prettierd,
+          yaml = formatters.lsp,
 
-        -- Use a tempfile instead of stdin
-        go = {
-          formatters.shell({
-            cmd = { "goimports-reviser", "-rm-unused", "-set-alias", "-format", "%" },
-            tempfile = function()
-              return vim.fn.expand("%") .. '.formatter-temp'
+          -- Add your own shell formatters:
+          myfiletype = formatters.shell({ cmd = { "myformatter", "%" } }),
+
+          -- Add lazy formatter that will only run when formatting:
+          my_custom_formatter = function()
+            if vim.api.nvim_buf_get_name(0):match("/README.md$") then
+              return formatters.prettierd
+            else
+              return formatters.lsp()
+            end
+          end,
+
+          -- Add custom formatter
+          filetype1 = formatters.remove_trailing_whitespace,
+          filetype2 = formatters.custom({
+            format = function(lines)
+              return vim.tbl_map(function(line)
+                return line:gsub("true", "false")
+              end, lines)
             end
           }),
-          formatters.shell({ cmd = { "gofmt" } }),
+
+          -- Concatenate formatters
+          python = {
+            formatters.remove_trailing_whitespace,
+            formatters.shell({ cmd = "tidy-imports" }),
+            formatters.black,
+            formatters.ruff,
+          },
+
+          -- Use a tempfile instead of stdin
+          go = {
+            formatters.shell({
+              cmd = { "goimports-reviser", "-rm-unused", "-set-alias", "-format", "%" },
+              tempfile = function()
+                return vim.fn.expand("%") .. '.formatter-temp'
+              end
+            }),
+            formatters.shell({ cmd = { "goimports" } }),
+            formatters.shell({ cmd = { "gofmt" } }),
+          },
         },
-      },
 
         -- Optional: fallback formatter to use when no formatters match the current filetype
         fallback_formatter = {
@@ -256,6 +259,25 @@ local plugins = {
   --   "mg979/vim-visual-multi",
   --   lazy = false,
   -- }
+  {
+    "nvimdev/lspsaga.nvim",
+    event = "LspAttach",
+    ft = { 'c', 'cpp', 'lua', 'rust', 'go' },
+    config = function()
+      require("lspsaga").setup({})
+    end,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter", -- optional
+      "nvim-tree/nvim-web-devicons",     -- optional
+    }
+  },
+  {
+    "chaoren/vim-wordmotion",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    event = "BufRead",
+    version = "*",
+  },
+
 }
 
 return plugins
